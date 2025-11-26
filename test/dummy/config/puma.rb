6fe6340d46ce2b937dd108b3ac5093e8c97b7179
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2025-11-26 18:44:46 -0500

    Puma update

--- test/dummy/config/puma.rb
+++ test/dummy/config/puma.rb
@@ -2,35 +2,27 @@
 
 # Puma configuration for shadcn-rails documentation site
 
-# Specifies the number of `workers` to boot in clustered mode.
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
-
-# Specifies the `port` that Puma will listen on to receive requests.
-# Bind to 0.0.0.0 in production for Fly.io
-if ENV["RAILS_ENV"] == "production"
-  bind "tcp://0.0.0.0:#{ENV.fetch('PORT') { 3000 }}"
-else
-  port ENV.fetch("PORT") { 3000 }
-end
-
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
-# Allow puma to be restarted by `bin/rails restart` command.
-plugin :tmp_restart
-
-# Run the Puma worker in single-process mode for lower memory in production
-# (documentation site doesn't need high concurrency)
+# Bind to 0.0.0.0 in production for Fly.io
 if ENV["RAILS_ENV"] == "production"
+  bind "tcp://0.0.0.0:#{ENV.fetch('PORT') { 3000 }}"
+  
+  # Single-process mode for lower memory (documentation site doesn't need high concurrency)
   workers 0
   threads_count = ENV.fetch("RAILS_MAX_THREADS") { 3 }
   threads threads_count, threads_count
   preload_app!
+else
+  # Development settings
+  port ENV.fetch("PORT") { 3000 }
+  workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+  worker_timeout 3600
+  
+  # Specifies the `pidfile` that Puma will use.
+  pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+  
+  # Allow puma to be restarted by `bin/rails restart` command.
+  plugin :tmp_restart
 end
