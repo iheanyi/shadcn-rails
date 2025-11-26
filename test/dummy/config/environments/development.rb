142e2f02c85a5f1432ef3e0244285a2de39440c3
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2025-11-26 14:38:05 -0500

    Update config to disable caching for faster iteration

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -10,13 +10,18 @@ Rails.application.configure do
   # Enable reloading of code in development
   config.enable_reloading = true
 
+  # Explicitly disable template caching for faster development iteration
+  config.action_view.cache_template_loading = false
+
   # ViewComponent configuration
   config.view_component.generate.preview = true
   config.view_component.show_previews = true
   config.view_component.preview_route = "/rails/view_components"
 
-  # Hotwire Livereload - watch parent gem's components directory
+  # Hotwire Livereload - watch parent gem's components directory and docs
   if defined?(Hotwire::Livereload)
     config.hotwire_livereload.listen_paths << Rails.root.join("../../app/components")
+    config.hotwire_livereload.listen_paths << Rails.root.join("app/views/docs")
+    config.hotwire_livereload.listen_paths << Rails.root.join("app/code_examples")
   end
 end
