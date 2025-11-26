0062f0cb6827b7a4880f9f629a5e2f7e85bd5777
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2025-11-26 18:30:26 -0500

    Fix up controller

--- test/dummy/app/controllers/component_previews_controller.rb
+++ test/dummy/app/controllers/component_previews_controller.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-# Only define this controller if ViewComponent::PreviewsController is available (development/test only)
-if defined?(ViewComponent::PreviewsController)
-  class ComponentPreviewsController < ViewComponent::PreviewsController
-    layout "component_preview"
-  end
+class ComponentPreviewsController < ViewComponent::PreviewsController
+  layout "component_preview"
 end
