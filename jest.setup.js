625e8da1a63fa351cbadeb512dccd64d026b32dc
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2025-11-27 01:04:18 -0500

    Fix up jest

--- jest.setup.js
+++ jest.setup.js
@@ -3,3 +3,6 @@ import '@testing-library/jest-dom'
 // Make jest globals available in ESM modules
 import { jest } from '@jest/globals'
 globalThis.jest = jest
+
+// Mock scrollIntoView since JSDOM doesn't implement it
+Element.prototype.scrollIntoView = jest.fn()
