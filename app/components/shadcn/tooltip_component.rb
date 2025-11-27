bb6269c16637a1b0d6b5973bf4bebacd6e25bb08
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2025-11-27 14:14:40 -0500

    Polish Tooltip animation

--- app/components/shadcn/tooltip_component.rb
+++ app/components/shadcn/tooltip_component.rb
@@ -18,7 +18,7 @@ module Shadcn
   #   <% end %>
   #
   class TooltipComponent < BaseComponent
-    CONTENT_CLASSES = "z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground whitespace-nowrap animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+    CONTENT_CLASSES = "z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground whitespace-nowrap animate-tooltip-in data-[state=closed]:animate-tooltip-out"
 
     # @param text [String] Tooltip text content
     # @param side [Symbol] Side to show tooltip (:top, :right, :bottom, :left)
